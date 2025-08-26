e65c39e4e41c6cf2de27b769ca9b3e868c2a9ab7
EdenArts <ojedaeden08@gmail.com>
2025-08-26 16:21:32 -0700

    Added data classes for the incoming data from chess API

--- cmd/cli/main.py
+++ cmd/cli/main.py
@@ -19,10 +19,11 @@ resp = requests.get(api_url, headers=headers, timeout=10)
 
 print("Status:", resp.status_code)
 if resp.ok:
-    out_path = Path("samples/players/output.txt")
-    out_path.parent.mkdir(parents=True, exist_ok=True)
-    out_path.write_text(json.dumps(resp.json()))
-    print(json.dumps(resp.json(), indent=2))
+    playerDataObject = playerData(**resp.json().get())
+    #out_path = Path("samples/players/output.txt")
+    #out_path.parent.mkdir(parents=True, exist_ok=True)
+    #out_path.write_text(json.dumps(resp.json()))
+    #print(json.dumps(resp.json(), indent=2))
 else:
     print(resp.text[:200])
 
