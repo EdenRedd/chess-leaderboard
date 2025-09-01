4091fb555cd4271dfa5ba50923b093bcc56687b5
Raul Ojeda <ojedaeden08@gmail.com>
2025-09-01 12:29:38 -0700

    Main script is connected to dynamodb table and uploads all player entries for the daily game

--- cmd/cli/main.py
+++ cmd/cli/main.py
@@ -23,13 +23,23 @@ resp = requests.get(api_url, headers=headers, timeout=10)
 
 print("Status:", resp.status_code)
 if resp.ok:
-    playerJson = resp.json().get("daily", [0])[0]
-    print(playerJson)
-    playerDataObject = playerData.from_json(playerJson)
+    playerJson = resp.json().get("daily")
+    for item in playerJson:
+        print(item)
+        playerDataObject = playerData.from_json(item)
 
-    table.put_item(Item={"hash_key": f"daily#{playerDataObject.country}", "range_key": f"{playerDataObject.rank}#{playerDataObject.id}" })
     
-    print(playerDataObject)
+        countryRequest = requests.get(playerDataObject.country, headers=headers, timeout=10)
+        countryResponseJson = countryRequest.json().get("code")
+        playerRequest = requests.get(playerDataObject.id, headers=headers, timeout=10)
+        print("================")
+        print(playerRequest)
+        playerResponseJson = playerRequest.json().get("player_id")
+
+
+        table.put_item(Item={"hash_key": f"daily#{countryResponseJson}", "range_key": f"{playerDataObject.rank}#{playerResponseJson}", "player_id": f"{playerResponseJson}", "url": f"{playerDataObject.url}", "username": f"{playerDataObject.username}", "score" : f"{playerDataObject.score}", "rank": f"{playerDataObject.rank}", "country": f"{playerDataObject.country}", "name": f"{playerDataObject.name}", "status": f"{playerDataObject.status}", "avatar": f"{playerDataObject.avatar}", "flair_code": f"{playerDataObject.flair_code}", "win_count": f"{playerDataObject.win_count}", "loss_count": f"{playerDataObject.loss_count}", "draw_count": f"{playerDataObject.draw_count}" })
+    
+    #print(playerDataObject)
 else:
     print(resp.text[:200])
 
