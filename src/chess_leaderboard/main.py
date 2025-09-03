f115057323b677a4ebbb66d6c961df720f6d2de3
EdenArts <ojedaeden08@gmail.com>
2025-09-02 21:29:03 -0700

    Added comments for seperating the logic and wrote main function that will serve as the central place where all scripts will run from on command

---
+++ src/chess_leaderboard/main.py
@@ -0,0 +1,8 @@
+from chess_leaderboard.services import leaderboard
+
+def main():
+    #Call the main logic from you services
+    # seperate your logic into the respective folders
+
+    if __name__ == "__main__":
+        main()
