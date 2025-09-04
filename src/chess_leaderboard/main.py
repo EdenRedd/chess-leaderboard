e05ab34af50f73d465d8053bd4508d64ab30bb98
EdenArts <ojedaeden08@gmail.com>
2025-09-03 23:07:45 -0700

    Started adding logic into appropriate files and attempting to run project from single point main

--- src/chess_leaderboard/main.py
+++ src/chess_leaderboard/main.py
@@ -1,8 +1,10 @@
 from chess_leaderboard.services import leaderboard
 
 def main():
-    #Call the main logic from you services
-    # seperate your logic into the respective folders
+    print("Entering main function")
+    players = leaderboard.fetch_chess_data()
+    print(players)
+    #leaderboard.store_players_to_dynamo(players)
 
     if __name__ == "__main__":
         main()
